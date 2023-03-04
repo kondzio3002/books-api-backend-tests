f3656e0adb9eb3488d2fef561533703c63704397
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-04 19:07:09 +0100

    Test 2,3

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -11,4 +11,16 @@ describe('CutText', () => {
     expect(cutText(function() {}, 20)).to.equal('Error');
   });
 
+  it('Should return an error if "content" arg length is 0', () => {
+    expect(cutText('', 20)).to.equal('Error');
+  });
+
+  it('Should return an error if "maxLength" arg is not a number', () => {
+    expect(cutText('Lorem Ipsum', undefined)).to.equal('Error');
+    expect(cutText('Lorem Ipsum', 'abc')).to.equal('Error');
+    expect(cutText('Lorem Ipsum', {})).to.equal('Error');
+    expect(cutText('Lorem Ipsum', [])).to.equal('Error');
+    expect(cutText('Lorem Ipsum', function() {})).to.equal('Error');
+  });
+
 });
